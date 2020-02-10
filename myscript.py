5df0318656b804f33b6d01e8c381aac4837b0b0a
zol bat <zolboo888@gmail.com>
2020-02-10 11:22:12 -0800

    added matrix function

--- myscript.py
+++ myscript.py
@@ -1,4 +1,11 @@
 import numpy as np
 
+
+def matrix(matrix_1, matrix_2):
+	return matrix_1 * matrix_2
+
+
 my_matrix = np.identity(4)
-print(my_matrix)
+my_matrix2 = np.random.random(size=(4,5))
+
+print(matrix(my_matrix, my_matrix2))
