1afe3130505383e78080eb45de9a5c1b72a69466
zol bat <zolboo888@gmail.com>
2020-02-10 10:58:03 -0800

    added printing odd numbers

--- new.py
+++ new.py
@@ -2,5 +2,10 @@ import numpy as np
 import pandas as pd
 
 
-l = [i for i in range(10) if i % 2 == 0]
-print(l)
+even = [i for i in range(10) if i % 2 == 0]
+print("even: ", even)
+
+
+odd = [i for i in range(10) if i % 2 != 0]
+
+print("odd: ", odd)
